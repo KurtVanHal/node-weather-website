d20339791e013216832159cb2b0bf38a8be3154e
Kurt Van Hal <kurt_van_hal@hotmail.com>
2019-03-10 11:00:35 +0100

    Setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,10 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+// Om de applicatie te laten werken op HEROKU, moet de correcte port worden
+// doorgegeven aan de app.listen methode. Deze is op de servers van heroku
+// te vinden binnen het process.env object. Lokaal wordt 3000 genomen
+const port = process.env.PORT || 3000
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
 const views = path.join(__dirname, '../templates/views');
@@ -30,28 +34,28 @@ app.get('', (req, res) => {
     // render zal de ingestelde view engine gebruiken om de pagina 
     // te genereren
     res.render('index', {
-        title:'Weather App',
-        name:"Kurt Van Hal"
+        title: 'Weather App',
+        name: "Kurt Van Hal"
     });
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
-        title:'About Me',
-        name:"Kurt Van Hal"
+        title: 'About Me',
+        name: "Kurt Van Hal"
     });
 })
 
 app.get('/help', (req, res) => {
     res.render('help', {
-        message:"This is the help page!",
-        title:'Help',
-        name:"Kurt Van Hal"
+        message: "This is the help page!",
+        title: 'Help',
+        name: "Kurt Van Hal"
     });
 })
 
 app.get('/weather', (req, res) => {
-    if(!req.query.address) {
+    if (!req.query.address) {
         return res.send({
             error: "You must provide an address"
         })
@@ -60,8 +64,12 @@ app.get('/weather', (req, res) => {
     // Belangrijk! default waarde geven aan het object. Indien dit niet 
     // wordt gedaan zal js het doorgegeven object willen destructurenn, hetgeen
     // niet mogelijk is indien undefined.
-    geocode(req.query.address, (error, {longitude, latitude, location} = {}) => {
-        if(error) {
+    geocode(req.query.address, (error, {
+        longitude,
+        latitude,
+        location
+    } = {}) => {
+        if (error) {
             return res.send({
                 error: error
             })
@@ -75,7 +83,7 @@ app.get('/weather', (req, res) => {
                     res.send({
                         forecast: forecastdata,
                         location,
-                        address:req.query.address
+                        address: req.query.address
                     })
                 }
             })
@@ -85,7 +93,7 @@ app.get('/weather', (req, res) => {
 });
 
 app.get('/products', (req, res) => {
-    if(!req.query.search) {
+    if (!req.query.search) {
         return res.send({
             error: 'You must provide a search term'
         })
@@ -98,20 +106,23 @@ app.get('/products', (req, res) => {
 
 app.get('/help/*', (req, res) => {
     res.render('404', {
-        message:"Help article not found",
-        title:'404',
-        name:"Kurt Van Hal"
+        message: "Help article not found",
+        title: '404',
+        name: "Kurt Van Hal"
     });
 })
 
 // Alles dat niet matcht
 app.get('*', (req, res) => {
     res.render('404', {
-        message:"Page not found",
-        title:'404',
-        name:"Kurt Van Hal"
-    })});
+        message: "Page not found",
+        title: '404',
+        name: "Kurt Van Hal"
+    })
+});
 
-app.listen(3000, () => {
-    console.log("Server is up on port 3000");
+
+//
+app.listen(port, () => {
+    console.log("Server is up on port " + port);
 });
